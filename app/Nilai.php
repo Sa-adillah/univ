2b3b6ce6a726529741573e8b8e5a6f8f2b68914b
Sa-adillah <lerox.3rd@gmail.com>
2021-07-21 16:36:47 +0800

    uas web2

--- app/Nilai.php
+++ app/Nilai.php
@@ -3,8 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Nilai extends Model
 {
-    //
+    protected $table            = 'nilai';
+    protected $fillable         = ['makul_id','mahasiswa_id','nilai'];
+    public    $timestamps       = false;
+
+    public function mahasiswa()
+    {
+        return $this->BelongsTo(Mahasiswa::class, 'mahasiswa_id', 'id');
+    }
+
+    public function makul()
+    {
+        return $this->belongsTo(Makul::class, 'makul_id', 'id');
+    }
 }
